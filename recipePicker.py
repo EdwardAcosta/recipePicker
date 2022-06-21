60ecce481dfea68fba63f5473efdcfa87aa1d740
Edward Rene Acosta <Eacosta2@satx.rr.com>
2022-06-20 23:02:02 -0500

    completed project

--- recipePicker.py
+++ recipePicker.py
@@ -6,8 +6,127 @@ import pyglet
 
 bg_colour = "#3d6466"
 
+pyglet.font.add_file("fonts/ubuntu-Bold.ttf")
+pyglet.font.add_file("fonts/Shanti-Regular.ttf")
+
+def clear_widgets(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+def fetch_db():
+    connection = sqlite3.connect("data/recipes.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM sqlite_schema WHERE type='table';")
+    all_tables= cursor.fetchall()
+
+    idx = random.randint(0,len(all_tables)-1)
+    
+    #fetch ingredients
+    table_name= all_tables[idx][1]
+
+    # don't forget to include Space between FROM " (closing quote)
+    cursor.execute("SELECT * FROM "+ table_name +";")
+    table_records = cursor.fetchall()
+
+    
+    #print(ingredients)
+    #print(table_name)
+    connection.close()
+    return table_name,table_records
+
+
+def pre_process(table_name,table_records):
+    title= table_name[:-6]
+    title= " ".join([char if char.islower() else " " + char for char in title])
+    print(title)
+
+    #ingredients
+    ingredients = []
+
+    for i in table_records:
+        name = i[1]
+        qty = i[2]
+        unit = i[3]
+        ingredients.append(qty + " " + unit + " Of " + name)
+
+    return title, ingredients
+
+
+def load_frame1():
+    clear_widgets(frame2)
+    frame1.tkraise()
+    frame1.pack_propagate(False)
+    #logo widget
+    logo_img=ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
+    logo_widget= tk.Label(frame1, image=logo_img,bg=bg_colour)
+    logo_widget.image = logo_img
+    logo_widget.pack()
+
+    #instructions widget
+    tk.Label(
+        frame1,
+        text="ready for your random recipe?",
+        bg=bg_colour,
+        fg="white",
+        font=("Shanti", 14)
+        ).pack()
+
+    # button widget
+    tk.Button(
+        frame1,
+        text="Shuffle",
+        font=("Ubuntu",20),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda:load_frame2()).pack(pady=20)
+
+
+
 def load_frame2():
-    print("Button has been pressed!")
+    clear_widgets(frame1)
+    frame2.tkraise()
+    table_name,table_records= fetch_db()
+    title, ingredients = pre_process(table_name,table_records)
+
+    #logo widget
+    logo_img=ImageTk.PhotoImage(file="assets/RRecipe_logo_bottom.png")
+    logo_widget= tk.Label(frame2, image=logo_img,bg=bg_colour)
+    logo_widget.image = logo_img
+    logo_widget.pack(pady=20)
+
+    tk.Label(
+        frame2,
+        text=title,
+        bg=bg_colour,
+        fg="white",
+        font=("Ubuntu", 20)
+        ).pack(pady=25)
+
+    for i in ingredients:
+        tk.Label(
+            frame2,
+            text=i,
+            bg="#28393a",
+            fg="white",
+            font=("Shanti", 12)
+            ).pack(fill="both")
+                
+    tk.Button(
+        frame2,
+        text="Back",
+        font=("Ubuntu",18),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda:load_frame1()
+        ).pack(pady=20)
+
 
 
 # initiallize app with basic settings
@@ -18,47 +137,19 @@ root.eval("tk::PlaceWindow . center")
 
 # place app in the center of the screen (alternative approach to root.eval())
 #x = root.winfo_screenwidth() // 2
-#y = int(root.winfo_screenheight() * 0.1)
+#y = int(root.winfo_screenheight() //2)
 #root.geometry('500x600+' + str(x) + '+' + str(y))
 
 
 frame1=tk.Frame(root, width=500, height=600,bg=bg_colour)
-frame1.grid(row=0,column=0)
-frame1.pack_propagate(False)
+frame2=tk.Frame(root, bg=bg_colour)
 
 
-#Frame1 widgets
-logo_img=ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
-logo_widget= tk.Label(frame1, image=logo_img,bg=bg_colour)
-logo_widget.image = logo_img
-logo_widget.pack()
+for frame in (frame1,frame2):
+    frame.grid(row=0,column=0, sticky="nesw")
 
-tk.Label(frame1,
- text="ready for your random recipe?",
- bg=bg_colour,
- fg="white",
- font=("TkMenuFont", 14)
- ).pack()
-
-# button widget
-tk.Button(
-    frame1,
-    text="Shuffle",
-    font=("TkHeadingFont",20),
-    bg="#28393a",
-    fg="white",
-    cursor="hand2",
-    activebackground="#badee2",
-    activeforeground="black",
-    command=lambda:load_frame2()).pack()
-    
-
-
-
-
-
-
-
+ 
+load_frame1()
 
 
 # run app
